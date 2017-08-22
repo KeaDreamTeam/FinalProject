c75b5a6284cd69fd92bcbd6aef708f2c41b300af
magda-gierg <mjgiergiczny@gmail.com>
2017-08-23 07:34:51 +1200

    map is not working

--- client/components/MainMap.jsx
+++ client/components/MainMap.jsx
@@ -1,12 +1,48 @@
 import React from 'react'
+import {Map, Marker} from 'google-maps-react';
 
-const MainMap = () => (
-  <div className="columns">
+class MainMap extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+          lat: null,
+          lng: null
+        }
+  }
+
+  render() {
+    return (
+      <div className="columns">
     <div className="column is-8 is-offset-2 box">
-      Hi I'm a map
-    </div>
+      <Map google={this.props.google}
+        style={{width: '50%', height: '50%', position: 'relative', margin: '10px'}}
+        className={'map'}
+        zoom={6}
+        initialCenter={{
+            lat: -36.8547512,
+            lng: 174.7787425
+          }}
+          >
+          {this.props.restaurants.map((restaurant, key) => {
+
+              console.log(this.props.restaurants);
+          this.geocoder = new google.maps.Geocoder
+            this.geocoder.geocode(
+            { address: restaurant.address },(results) => {
+               this.setState({lat: results[0].geometry.location.lat(), lng: results[0].geometry.location.lng()})
+          })
+          return(
+            <Marker key = {key} position={{lat: this.state.lat, lng: this.state.lng}} />
+          )
+  })}
+    </Map>
   </div>
-)
+</div>
+    );
+  }
+}
 
 
 export default MainMap
+
+// https://github.com/fullstackreact/google-maps-react
--- client/components/Restaurant.jsx
+++ client/components/Restaurant.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import {connect} from 'react-redux'
-
 import {getRestaurants} from '../actions/restaurants'
-
 import RestaurantList from './RestaurantList'
 import RestaurantSingle from './RestaurantSingle'
+import MainMap from './MainMap'
+
 
 class Restaurant extends React.Component {
   constructor (props) {
@@ -34,7 +34,7 @@ class Restaurant extends React.Component {
       <div className="columns">
         <div className="column is-8 is-offset-2 box">
           Restaurants!!!
-
+          <MainMap restaurants={this.props.restaurants} google={window.google}/>
           <RestaurantList restaurants={this.props.restaurants} toggleSingleView={this.state.toggleSingleView} singleView={this.state.singleView} select={this.selectRestaurant.bind(this)} />
             {this.state.selected && <RestaurantSingle selected={this.state.selected} select={this.selectRestaurant.bind(this)} /> }
         </div>
