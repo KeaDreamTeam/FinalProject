8149a982fe51f132be84c2bda183aca3a9627440
Usha <usha.macdonald@gmail.com>
2017-08-24 11:45:17 +1200

    main structure

---
+++ client/components/MainMap.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const MainMap = () => (
+  <div className="columns">
+    <div className="column is-8 is-offset-2 box">
+      Hi I'm a map
+    </div>
+  </div>
+)
+
+
+export default MainMap
