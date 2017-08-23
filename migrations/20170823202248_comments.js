4aa2a6e697d495f08003d39cd012c195bad770ee
Anna P <podhajskaa@gmail.com>
2017-08-23 21:01:45 +1200

    work in progress

--- migrations/20170823202248_comments.js
+++ migrations/20170823202248_comments.js
@@ -3,7 +3,8 @@ exports.up = function(knex, Promise) {
     table.increments('id')
     table.integer('restaurant_id')
     table.text('text')
-    table.integer('rating')
+    table.integer('rating_pos')
+    table.integer('rating_neg')
     table.timestamp('created_at').defaultTo(knex.fn.now())
   })
 };
@@ -11,3 +12,6 @@ exports.up = function(knex, Promise) {
 exports.down = function(knex, Promise) {
   return knex.schema.dropTableIfExists('comments')
 };
+
+
+//rating_pos & rating_pos have values 0/1 in comments
--- seeds/comments.js
+++ seeds/comments.js
@@ -6,9 +6,20 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex('comments').insert([
         {
-          id: 1, rating: '5',
-          pricing: "$$",
-    
+          id: 1,
+          restaurant_id: 1,
+          text: "Great food, low prices and nice staff",
+          rating_pos: 1,
+          rating_neg: 0,
+          created_at: 12/8/2017
+        },
+        {
+          id: 2,
+          restaurant_id: 1,
+          text: "Nice food, large portions",
+          rating_pos: 1,
+          rating_neg: 0,
+          created_at: 13/8/2017
         },
       ]);
     });
