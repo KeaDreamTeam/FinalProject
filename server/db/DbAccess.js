da23b218a1df1def5497c247f6fa914c44b46893
Anna P <podhajskaa@gmail.com>
2017-08-24 21:30:02 +1200

    added routes matching documentation, login route todo

--- server/db/DbAccess.js
+++ server/db/DbAccess.js
@@ -14,21 +14,14 @@ const getComments = (db) => {
     .select('*')
 }
 
+const saveComment = (comment, db) => {
+  return db('comments')
+    .insert(comment)
+}
+
 module.exports = {
   getRestaurants,
   getComments,
-  getComments_byRest
+  getComments_byRest,
+  saveComment
 }
-
-
-
-
-// const getDailyMoodById = (id, db) => {
-//   return db('dailymoods')
-//     .join('users', 'user_id', '=', 'users.id')
-//     .join('moods', 'mood_id', '=', 'moods.id')
-//     .join('locations', 'location_id', '=', 'locations.id')
-//     .select('username', 'mood_name', 'mood_type', 'mood_icon', 'location_name', 'mood_color', 'created_at')
-//     .where('users.id', id)
-//     // .first()
-// }
