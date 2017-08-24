55eb771f8162f944163b45d3e0df12cee3ecc341
Ania Podhajska <podhajskaa@gmail.com>
2017-08-24 19:00:05 +1200

    Update index.jsx
    merge conflict solved

--- client/reducers/index.js
+++ client/reducers/index.js
@@ -1,10 +1,6 @@
 import {combineReducers} from 'redux'
-
-<<<<<<< HEAD
-// import greetings from './greetings' 
-=======
 import restaurants from './restaurants'
->>>>>>> master
+
 
 export default combineReducers({
   restaurants
