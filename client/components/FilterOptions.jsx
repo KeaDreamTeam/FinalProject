5e9cfaba22b7f6630373a6918649e7b3bc65975c
Anna P <podhajskaa@gmail.com>
2017-08-26 13:46:11 +1200

    buttons added

--- client/components/FilterOptions.jsx
+++ client/components/FilterOptions.jsx
@@ -1,15 +1,57 @@
 import React from 'react'
 
-const FilterOptions = props => {
-  return (
-    <div>
-      Filter Options
-      <br />
-      <a href='#' onClick={props.toggleFilters}>
-        Hide Filters
-      </a>
-    </div>
-  )
+class FilterOptions extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      filterOptions: [
+        {filter_name: "is_meat", filter_value: true, filter_displayname: "has meat options"},
+        {filter_name: "is_vegan", filter_value: true, filter_displayname: "has vegan options"},
+        {filter_name: "is_breakfast", filter_value: true, filter_displayname: "has breakfast"},
+        {filter_name: "is_dinner", filter_value: true, filter_displayname: "has dinner options"},
+        {filter_name: "is_dinein", filter_value: true, filter_displayname: "dine in"},
+        {filter_name: "is_takeaway", filter_value: true, filter_displayname: "take away"}
+      ]
+    }
+  }
+
+  render() {
+    return (
+      <div className="filterOptions-container">
+
+        <div className="toggle">
+          Filter Options
+          <br />
+          <a href='#' onClick={this.props.toggleFilters}>
+            Hide Filters
+          </a>
+        </div>
+
+        <div className="columns is-multiline">
+
+            {this.renderFilterOptions()}
+
+        </div>
+
+      </div>
+    )
+  }
+
+  renderFilterOptions() {
+    return (
+      this.state.filterOptions.map((option, i) => <div  className="column is-half filterButton" key={i}>{option.filter_displayname}</div> )
+    )
+  }
+
+
 }
+
+
+
+// const mapStateToProps = (state) => {
+//   return {restaurants: state.restaurants}
+// }
+
+
 
 export default FilterOptions
