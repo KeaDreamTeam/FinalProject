65cbb0f2c5b5aa26ec94bd77db270a3864291b49
magda-gierg <mjgiergiczny@gmail.com>
2017-08-26 13:28:26 +1200

    onClick added

--- client/components/MainMap.jsx
+++ client/components/MainMap.jsx
@@ -1,17 +1,41 @@
 import React from 'react'
-import {Map, Marker} from 'google-maps-react';
+import {Map, Marker, InfoWindow} from 'google-maps-react';
 class MainMap extends React.Component {
   constructor (props) {
     super(props)
+    this.state = {
+      showingInfoWindow: false,
+      activeMarker: {},
+      selectedPlace: {},
+    }
+    this.onMarkerClick = this.onMarkerClick.bind(this);
+   this.onMapClicked = this.onMapClicked.bind(this);
   }
 
+  onMarkerClick(props, marker, e) {
+      this.setState({
+        selectedPlace: props,
+        activeMarker: marker,
+        showingInfoWindow: true
+      });
+    }
+
+    onMapClicked(props) {
+      if (this.state.showingInfoWindow) {
+        this.setState({
+          showingInfoWindow: false,
+          activeMarker: null
+        })
+      }
+    }
 
   render() {
     return (
       <div className="columns">
         <div className="column is-8 is-offset-2 box">
-          <Map google={this.props.google}
-            style={{width: '40%', height: '60%', position: 'relative', margin: '10px'}}
+          <Map google={window.google}
+            style={{width: '30%', height: '50%', position: 'relative', margin: '10px'}}
+            onClick={this.onMapClicked}
             className={'map'}
             zoom={12}
             initialCenter={{
@@ -19,9 +43,23 @@ class MainMap extends React.Component {
               lng: 174.77488518
             }}
             >
-            {this.props.restaurants.map((restaurant, key) => {
-              return <Marker key={key} position={{lat:restaurant.lat, lng:restaurant.lng}} />
-            })}
+            {this.props.restaurants.map((restaurant, key) =>
+              <Marker
+                key={key}
+                title={restaurant.restaurant_name}
+                name={restaurant.restaurant_name}
+                description={restaurant.address}
+                onClick={this.onMarkerClick}
+                position={{lat:restaurant.lat, lng:restaurant.lng}} />
+            )}
+            <InfoWindow
+              marker={this.state.activeMarker}
+              visible={this.state.showingInfoWindow}>
+              <div>
+                <h1>{this.state.selectedPlace.name}</h1>
+                <p>{this.state.selectedPlace.description}</p>
+              </div>
+            </InfoWindow>
           </Map>
         </div>
       </div>
