6ad0de4d25b3642783307fd1e6b318d6d6161866
Anna P <podhajskaa@gmail.com>
2017-08-26 18:29:15 +1200

    filter works

--- client/reducers/restaurants.js
+++ client/reducers/restaurants.js
@@ -2,9 +2,9 @@ function restaurants (state = [], action) {
   switch(action.type) {
     case 'RECEIVE_RESTAURANTS':
       return [...action.restaurants]
-    case 'UPDATE_FILTER':
-      console.log(state, action);
-      return state
+    // case 'UPDATE_FILTER':
+    //   console.log('restaurants', state, action)
+    //   return state.filterOptions
     default:
       return state
   }
