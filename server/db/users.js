6d4fa85496ac7daeb541231538d65fd58a38d5cb
Usha <usha.macdonald@gmail.com>
2017-08-26 15:07:53 +1200

    sat pm

---
+++ server/db/users.js
@@ -0,0 +1,35 @@
+const hash = require('../auth/hash')
+
+
+module.exports = {
+  createUser,
+  userExists,
+  getUserByName
+}
+
+function createUser (user_name, password, conn) {
+  const passwordHash = hash.generate(password)
+  console.log("create user", {user_name, password});
+  const db = conn
+  return db('users')
+    .insert({user_name, hash: passwordHash})
+    console.log(hash);
+}
+
+function userExists (user_name, conn) {
+  const db = conn
+  return db('users')
+    .count('id as n')
+    .where('user_name', user_name)
+    .then(count => {
+      return count[0].n > 0
+    })
+}
+
+function getUserByName (username, conn) {
+  const db = conn
+  return db('users')
+    .select()
+    .where('username', username)
+    .first()
+}
---
+++ server/routes/auth.js
@@ -0,0 +1,27 @@
+const express = require('express')
+
+const router = express.Router()
+
+const {userExists, createUser} = require('../db/users')
+const token = require('../auth/token')
+
+
+router.post('/register', register, token.issue)
+
+function register (req, res) {
+  const {user_name, password} = req.body
+  userExists(user_name, req.app.get('db'))
+    .then(exists => {
+      if(exists) {
+        return res.status(400).send({message: 'User exists'})
+      }
+      createUser(user_name, password, req.app.get('db'))
+        .then(() => res.status(201).end())
+    })
+    .catch(err => {
+      res.status(500).send({message: err.message})
+    })
+
+}
+
+module.exports = router
