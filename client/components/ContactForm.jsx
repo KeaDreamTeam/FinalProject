ee84aca35417ce922e28d0c47cb4abbc914a9ed9
magda-gierg <mjgiergiczny@gmail.com>
2017-08-27 18:50:32 +1200

    contactForm is working

--- client/components/ContactForm.jsx
+++ client/components/ContactForm.jsx
@@ -1,30 +1,21 @@
 import React from 'react'
 
-
-class ContactForm extends React.Component {
-  constructor (props) {
-    super(props)
-    this.state = {
-      form: {},
-      submitted: false,
-      message: '',
-      errorMessage: ''
-    }
-  }
-
-  render () {
-    return (
-      <div className = 'wallpaper-no-border'>
-        <div className='container contact'>
-          <form method="POST" action="http://formspree.io/mjgiergiczny@gmail.com">
-            <input type="email" name="email" placeholder="Your email" />
-            <textarea name="message" placeholder="Your message"></textarea>
-            <button type="submit">Send</button>
-          </form>
-
-        </div>
-    )
-  }
+const ContactForm = ({toggleContactForm}) => {
+  return (
+    <div className = 'wallpaper-no-border'>
+      <div className='container contact'>
+        <form method="POST" action="http://formspree.io/mjgiergiczny@gmail.com">
+          <input type="email" name="email" placeholder="Your email" />
+          <textarea name="message" placeholder="Your message"></textarea>
+          <button type="submit">Send</button>
+        </form>
+      </div>
+      <a href='#' onClick={toggleContactForm}>
+        Hide ContactForm
+      </a>
+    </div>
+  )
 }
+
 
 export default ContactForm
