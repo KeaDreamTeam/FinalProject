b355fb4fdde18e489a8f0913e6c6522fe8686b56
Anna P <podhajskaa@gmail.com>
2017-08-27 22:50:17 +1200

    UI touch up

--- client/components/RestaurantSingle.jsx
+++ client/components/RestaurantSingle.jsx
@@ -7,16 +7,27 @@ import RatingCommentAdd from './RatingCommentAdd'
 
 const RestaurantSingle = ({selected, select}) => {
   return (
-    <div>
+    <div className="RestaurantSingle-container">
+      <h1 className="restaurantName">{selected.restaurant_name}</h1>
       <Ratings restaurantId={selected.restaurant_id} />
-      <h1>{selected.restaurant_name}</h1>
-      <p>{selected.address}</p>
-      <p>{selected.phone}</p>
+      <h3 className="restaurantDescription">Restaurant description</h3>
       <p>{selected.description}</p>
-      <img src={selected.image_url} />
+      <img className="restaurantPhoto" src={selected.image_url} />
       <MapSingle restaurant={selected} />
-      <RatingCommentAdd user_id={500} restaurant_id={selected.restaurant_id} />
-      <CommentsView restaurantId={selected.restaurant_id} />
+      <div className="restaurantContact-container">
+        <h1 className="Address-title">Address</h1>
+        <p className="address">{selected.address}, <a href={selected.phone}>{selected.phone}</a> </p>
+      </div>
+      <div className="restaurantComments-container">
+        <h1 className="Comments-title">Comments</h1>
+        <div className="Comments-list">
+          <CommentsView restaurantId={selected.restaurant_id} />
+        </div>
+      </div>
+      <div lassName="restaurantAddComment-container">
+        <h1 className="AddComment-title">Add comment and rating</h1>
+          <RatingCommentAdd user_id={500} restaurant_id={selected.restaurant_id} />
+      </div>
       <a href="#" onClick={() => select(null)}>Close</a>
     </div>
   )
