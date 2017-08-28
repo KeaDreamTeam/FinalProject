2c65a8a1606d9b9f20961bd7db4a9379dbcefc92
magda-gierg <mjgiergiczny@gmail.com>
2017-08-28 16:14:48 +1200

    routes.js tested

--- test/server/server.test.js
+++ test/server/server.test.js
@@ -18,6 +18,43 @@ test.cb('GET / restaurants returns restaurants', t => {
     })
 })
 
+test.cb('GET / restaurant comment return comment', t => {
+  request(t.context.app)
+    .get('/api/v1/restaurants/2/comments')
+    .expect(200)
+    .end((err,res) => {
+      if (err) console.log(err);
+      t.is(res.body[3].content, "good price and great food")
+      t.end()
+    })
+})
+
+test.cb('GET / restaurant rating return positive_vote', t => {
+  request(t.context.app)
+    .get('/api/v1/restaurants/3/ratings')
+    .expect(200)
+    .end((err,res) => {
+      if (err) console.log(err);
+      t.true(res.body.positive_vote >= 0)
+      t.true(res.body.negative_votes >= 0)
+      t.end()
+    })
+})
+
+test.cb('GET / each commen return specific content', t => {
+  request(t.context.app)
+    .get('/api/v1/comments')
+    .expect(200)
+    .end((err,res) => {
+      if (err) console.log(err);
+      t.is(res.body[1].content, "all ok!")
+      t.end()
+    })
+})
+
+
+// tests ->DB
+
 test.cb('read restaurants db', t => {
   restaurantsDb.getRestaurants(t.context.db)
     .then(restaurants => {
@@ -34,16 +71,7 @@ test.cb('read comments db', t => {
     })
 })
 
-test.cb('GET / comments', t => {
-  request(t.context.app)
-    .get('/api/v1/comments')
-    .expect(200)
-    .end((err,res) => {
-      if (err) console.log(err);
-      t.is(comments.length, 5)
-      t.end()
-    })
-})
+
 
 
 
