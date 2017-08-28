72dee28ebe70e8672c64b44507b3ddcd9b2eb299
Usha <usha.macdonald@gmail.com>
2017-08-28 14:26:51 +1200

    changes

--- client/components/RestaurantList.jsx
+++ client/components/RestaurantList.jsx
@@ -5,16 +5,21 @@ import {getRestaurants} from '../actions/restaurants'
 
 const RestaurantList = ({restaurants, toggleSingleView, singleView, select}) => {
   return (
-    <div className="columns is-multiline">
-      {restaurants.map((restaurant, i) => {
-        return (
-          <div className="column is-half" key={i}>
-            {restaurant.restaurant_name}
-            <a onClick={() => select(restaurant) }>Details</a>
+    <div className="hero is-fullheight">
+      <div className="columns">
+        <div className="column is-10 is-offset-1">
+          <div className="columns is-multiline restaurant-container">
+            {restaurants.map((restaurant, i) => {
+              return (
+                <div className="has-text-centered column is-half box" key={i}>
+                  <a onClick={() => select(restaurant) }>{restaurant.restaurant_name}</a>
+                </div>
+              )
+            })}
           </div>
-        )
-      })}
-    </div>
+        </div>
+      </div>
+  </div>
   )
 }
 
