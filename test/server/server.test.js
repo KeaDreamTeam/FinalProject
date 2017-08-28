fc412bf498595664fae1cc7668dc556feec524cc
magda-gierg <mjgiergiczny@gmail.com>
2017-08-28 16:31:37 +1200

    7 tests passed

--- test/server/server.test.js
+++ test/server/server.test.js
@@ -7,6 +7,9 @@ var setupDb = require('./setup-db')
 
 setupDb(test,createServer)
 
+
+// tests routes/routes.js
+
 test.cb('GET / restaurants returns restaurants', t => {
   request(t.context.app)
     .get('/api/v1/restaurants')
@@ -72,29 +75,29 @@ test.cb('read comments db', t => {
 })
 
 
+// tests routes/auth.js
 
+test.cb('POST /register ', t => {
+  process.env.JWT_SECRET = 'secret'
+  const newUser = {
+    user_name: 'Magda123',
+    name: 'Magda',
+    password: 'testpassword'
+  }
 
+  const originalCount = 7
 
-// test.only.cb('POST /register ', t => {
-//   process.env.JWT_SECRET = 'secret'
-//   const newUser = {
-//     username: 'testuser',
-//     name: 'bob bob',
-//     password: 'testpassword'
-//   }
-//
-//   const originalCount = 11
-//
-//   request(t.context.server)
-//     .post('/api/register')
-//     .send(newUser)
-//     .expect(200)
-//     .end((err, res) => {
-//       if (err) throw err
-//       t.context.connection('members')
-//       .then((members) => {
-//         t.is(members.length, originalCount + 1)
-//         t.end()
-//       })
-//     })
-// })
+  request(t.context.app)
+    .post('/api/v1/auth/register')
+    .send(newUser)
+    .expect(200)
+    .end((err, res) => {
+      if (err) throw err
+      t.true(res.body.hasOwnProperty('token'))
+      t.context.db('users')
+      .then((users) => {
+        t.is(users.length, originalCount + 1)
+        t.end()
+      })
+    })
+})
