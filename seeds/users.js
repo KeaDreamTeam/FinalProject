32af42f9e72e7b9c979955539388a63bbcb55aaa
Anna P <podhajskaa@gmail.com>
2017-08-30 10:50:02 +1200

    changed user seeds

--- seeds/users.js
+++ seeds/users.js
@@ -6,12 +6,12 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex('users').insert([
         {user_id: 1, user_name: 'Bob', hash: "adsew3r23rdsds"},
-        {user_id: 2, user_name: 'Jan', hash: "adscxvcxvds"},
-        {user_id: 3, user_name: 'Harrison', hash: "adsew3r23rdsds"},
-        {user_id: 4, user_name: 'Ania', hash: "adsew3r2ddvdxv3rdsds"},
-        {user_id: 5, user_name: 'Ula', hash: "cvxxzcxzcxzccv"},
-        {user_id: 6, user_name: 'Usha', hash: "adsew3rcxvcxv23rdsds"},
-        {user_id: 7, user_name: 'Magda', hash: generate('password')}
+        {user_id: 2, user_name: 'Matthew', hash: "adscxvcxvds"},
+        {user_id: 3, user_name: 'Liam', hash: "adsew3r23rdsds"},
+        {user_id: 4, user_name: 'Tina', hash: "adsew3r2ddvdxv3rdsds"},
+        {user_id: 5, user_name: 'Ella', hash: "cvxxzcxzcxzccv"},
+        {user_id: 6, user_name: 'Harper', hash: "adsew3rcxvcxv23rdsds"},
+        {user_id: 7, user_name: 'Mia', hash: generate('password')}
       ]);
     });
 };
