17a1d7100f2b8569c9d3b1c9b902e0406728afb6
Anna P <podhajskaa@gmail.com>
2017-08-30 17:20:18 +1200

    polished header added pointer

--- client/components/HeaderBody.jsx
+++ client/components/HeaderBody.jsx
@@ -5,7 +5,7 @@ import jump from 'jump.js'
 const HeaderBody = () => (
   <div>
     <h1 className="title is-1" onClick={() => jump('.app-body')}>
-      What's For Lunch?
+      <a>What's For Lunch?</a>
     </h1>
     <h2 className="animated bounce ClickMe" onClick={() => jump('.app-body')}>
       <a>click me</a>
