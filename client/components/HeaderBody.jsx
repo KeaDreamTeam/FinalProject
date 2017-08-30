8f3198981a32450aafc5d16f646e2d442cb554d3
Anna P <podhajskaa@gmail.com>
2017-08-30 17:15:09 +1200

    added clickable header

--- client/components/HeaderBody.jsx
+++ client/components/HeaderBody.jsx
@@ -4,7 +4,7 @@ import jump from 'jump.js'
 
 const HeaderBody = () => (
   <div>
-    <h1 className="title is-1">
+    <h1 className="title is-1" onClick={() => jump('.app-body')}>
       What's For Lunch?
     </h1>
     <h2 className="animated bounce ClickMe" onClick={() => jump('.app-body')}>
