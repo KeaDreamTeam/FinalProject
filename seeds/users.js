d4114126e2825cc3965cb237f2466b6d3c31b533
harrison-symes <symeshjb@gmail.com>
2017-09-12 18:25:38 +1200

    fixed user seeds for heroku

--- seeds/users.js
+++ seeds/users.js
@@ -6,7 +6,7 @@ exports.seed = function(knex, Promise) {
       // Inserts seed entries
       return knex('users').insert([
         {user_name: 'Bob', hash: "adsew3r23rdsds"},
-        {user_name: 'Matthew', hash: "adscxvcxvds"},
+        { user_name: 'Matthew', hash: "adscxvcxvds"},
         {user_name: 'Mick', hash: "adsew3r23rdsds"},
         {user_name: 'Ella', hash: "adsew3r2ddvdxv3rdsds"},
         {user_name: 'Ula', hash: "cvxxzcxzcxzccv"},
